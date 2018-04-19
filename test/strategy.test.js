8a8d4bbf407ba7a84528c663c92ba6eada6a08f0
Ing. Andrej Poliak <andrej.poliak.ada@gmail.com>
2018-04-19 12:50:49 +0200

    GrantType scope from configuration

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -5,7 +5,7 @@ var should = require('should');
 describe('auth0 strategy', function () {
   before(function () {
     this.strategy = new Auth10Strategy({
-       domain:       'jj.auth0.com', 
+       domain:       'jj.auth0.com',
        clientID:     'testid',
        clientSecret: 'testsecret',
        callbackURL:  '/callback'
@@ -18,12 +18,12 @@ describe('auth0 strategy', function () {
     this.strategy.options
       .authorizationURL.should.eql('https://jj.auth0.com/authorize');
   });
-  
+
   it('tokenURL should have the domain', function () {
     this.strategy.options
       .tokenURL.should.eql('https://jj.auth0.com/oauth/token');
   });
-  
+
   it('userInfoURL should have the domain', function () {
     this.strategy.options
       .userInfoURL.should.eql('https://jj.auth0.com/userinfo');
@@ -87,9 +87,9 @@ describe('auth0 strategy', function () {
 
   });
 
-  describe('authenticate', function () { 
+  describe('authenticate', function () {
     it('when there is an error querystring propagate', function (done) {
-      
+
       this.strategy.fail = function (challenge, status) {
         challenge.should.eql('domain_mismatch');
         done();
@@ -101,7 +101,7 @@ describe('auth0 strategy', function () {
         }
       });
     });
-  }); 
+  });
 });
 
 describe('auth0 strategy with state parameter disabled', function () {
