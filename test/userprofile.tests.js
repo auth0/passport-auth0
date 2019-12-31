07f95dc4a8d294d8810d9c257d6afe2a549bf2b8
Josh Cunningham <josh@joshcanhelp.com>
2019-12-31 14:54:53 -0800

    Fix missing ID causing cannot read error
    The id of the profile should be a string but if the openid
    scope is not used during login, a profile can be returned
    but not parsed by this strategy. Developers should use
    the openid scope but this strategy should enforce that
    at startup, not fail during callback.

--- test/userprofile.tests.js
+++ test/userprofile.tests.js
@@ -3,34 +3,66 @@ var auth0ProfileNoIdentites = require('./fixtures/auth0-example-profile-no-ident
 var auth0OIDCProfile = require('./fixtures/auth0-oidc-example-profile');
 var Profile = require('../lib/Profile');
 
-describe('Profile', function () {
+describe('Standard profiles', function () {
   [auth0Profile, auth0ProfileNoIdentites, auth0OIDCProfile].forEach(function(profile) {
     before(function () {
       this.profile = new Profile(profile);
     });
-  
+
     [
-      ['provider', 'google-oauth2'],
-      ['id', 'google-oauth2|ddddd123123123'],
-      ['user_id', 'google-oauth2|ddddd123123123'],
-      ['displayName', 'José F. Romaniello']
+      ['provider', '__test_provider__'],
+      ['id', '__test_provider__|__test_user_id__'],
+      ['user_id', '__test_provider__|__test_user_id__'],
+      ['displayName', '__test_name__']
     ].forEach(function(tuple) {
       it('should map ' + tuple[0], function () {
         this.profile[tuple[0]]
           .should.eql(tuple[1]);
       });
     });
-  
+
     it('should map the name', function () {
       this.profile.name.givenName
-          .should.eql('José F.');
+          .should.eql('__test_given_name__');
       this.profile.name.familyName
-          .should.eql('Romaniello');
+          .should.eql('__test_family_name__');
     });
-  
-    it('should map the emails', function () { 
+
+    it('should map the emails', function () {
       this.profile.emails[0]
-        .value.should.eql('jfromaniello@gmail.com');
+        .value.should.eql('__test_email__');
     });
   });
 });
+
+describe('Non-standard profiles', function () {
+
+  describe('Profile without an ID', function () {
+    const profile = new Profile({email: '__test_email__'});
+
+    it('should have no provider', function () {
+      profile.should.not.have.property('provider');
+    });
+  });
+
+  describe('Profile without a provider in the user ID', function () {
+    const profile = new Profile({sub: '__test_sub__'});
+
+    it('should have no provider', function () {
+      profile.should.not.have.property('provider');
+    });
+
+    it('should have the correct ID', function () {
+      profile.id.should.eql('__test_sub__');
+    });
+  });
+
+  describe('Profile without an email', function () {
+    const profile = new Profile({sub: '__test_sub__'});
+
+    it('should have no emails', function () {
+      profile.should.not.have.property('emails');
+    });
+  });
+});
+
